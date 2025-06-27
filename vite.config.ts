3d6a6ad1c33b24987e49edea3c2d070873e52502
PrinoyDcosta <78203778+PrinoyDcosta@users.noreply.github.com>
2025-06-28 01:36:04 +0530

    Added image optimizer in vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ViteImageOptimizer({
+     png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 50,
+  },
+  jpeg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 50,
+  },
+  jpg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 50,
+  },
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
