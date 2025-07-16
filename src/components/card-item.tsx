902bd46ac77b2b57a9e3bd7fe713bcb513dcb9b3
Prinoy D'Costa <78203778+PrinoyDcosta@users.noreply.github.com>
2025-07-16 04:35:43 +0400

    Fixed project card height issue

--- src/components/card-item.tsx
+++ src/components/card-item.tsx
@@ -4,19 +4,17 @@ import { GlowingEffect } from "./ui/glowing-effect";
 import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 interface CardItemProps {
-  area: string;
   icon: string;
   title: string;
   description: React.ReactNode;
   projectLink?: string;
   githubLink?: string;
   tags?: string[]
-
 }
 
-const CardItem = ({ area, icon, title, description, projectLink, tags = [], githubLink }: CardItemProps) => {
+const CardItem = ({ icon, title, description, projectLink, tags = [], githubLink }: CardItemProps) => {
   return (
-    <li className={`md:min-h-[14rem] list-none w-full md:w-1/3 xl:w-1/4 ${area}`}>
+    <li>
       <div className="relative h-full rounded-2xl border p-2 md:rounded-3xl md:p-3">
         <GlowingEffect
           blur={0}
@@ -29,20 +27,25 @@ const CardItem = ({ area, icon, title, description, projectLink, tags = [], gith
         />
         <div className="border-0.75 relative flex h-full flex-col justify-between gap-6 overflow-hidden rounded-xl p-6 md:p-6 dark:shadow-[0px_0px_27px_0px_#2D2D2D]">
           <div className="relative flex flex-1 flex-col justify-between gap-3">
-            <div className="w-fit rounded-lg border border-gray-600 p-2">
-              <img src={icon} className="h-16 w-16 text-black dark:text-neutral-400"/>
+            <div>
+              <div className="w-fit rounded-lg border border-gray-600 p-2">
+                <img src={icon} className="h-16 w-16 text-black dark:text-neutral-400"/>
+              </div>
+              <div className="space-y-3 mt-2">
+                <h3 className="-tracking-4 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance text-black md:text-2xl/[1.875rem] dark:text-white">
+                  {title}
+                </h3>
+                <h2 className="font-sans text-sm/[1.125rem] text-black md:text-base/[1.375rem] dark:text-neutral-400 [&_b]:md:font-semibold [&_strong]:md:font-semibold">
+                  {description}
+                </h2>
+              </div>
+
             </div>
-            <div className="space-y-3">
-              <h3 className="-tracking-4 pt-0.5 font-sans text-xl/[1.375rem] font-semibold text-balance text-black md:text-2xl/[1.875rem] dark:text-white">
-                {title}
-              </h3>
-              <h2 className="font-sans text-sm/[1.125rem] text-black md:text-base/[1.375rem] dark:text-neutral-400 [&_b]:md:font-semibold [&_strong]:md:font-semibold">
-                {description}
-              </h2>
+            <div>
               <div className="flex gap-2">
                 {tags.map(item => <RainbowButton variant={"outline"}>{item}</RainbowButton>)}
               </div>
-              <div className="flex gap-2 w-fit">
+              <div className="flex gap-2 w-fit mt-3">
                 {projectLink &&
                  <Tooltip>
                   <TooltipTrigger asChild>
--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -18,9 +18,8 @@ const Projects = () => {
                 A few projects I've designed, built, and optimized.
             </p>
         </div>
-        <ul className="flex gap-5 items-center justify-center flex-wrap">
+        <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 ">
             <CardItem
-                area="md:[grid-area:2/1/3/7] xl:[grid-area:1/5/3/8]"
                 icon={GotoIcon}
                 title="GoTo Unified Checkout Flow - GoTo Technologies, USA"
                 description="Built a B2B SaaS e-commerce Dynamic Checkout Flow using React, TypeScript, and Sitecore, enabling web publishers to configure it effortlessly without developer intervention. Designed customizable sections to tailor the checkout experience for different products. Implemented granular configuration options, allowing unique setups for each product. Integrated GA tracking to monitor performance and user behavior, aiding data-driven growth strategies. Reduced development time by empowering publishers with self-service controls."
@@ -28,7 +27,6 @@ const Projects = () => {
                 projectLink="https://buy.logmein.com/buy/resolve/standard/customize"
             />    
               <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={GotoIcon}
                 title="GoTo Forms widget - GoTo Technologies, USA"
                 description="Built a CMS-driven, customizable form component that empowers web publishers to create and manage forms without developer dependency. Supports dynamic field configuration (text, dropdowns, checkboxes, etc.) directly from the CMS. Developed with React and Formik for seamless form handling and validation. Tracks key Google Analytics events—form views, submissions, and abandonment—for actionable insights. Optimized for flexibility, allowing quick form updates via the CMS. Reduces dev overhead while maintaining robust functionality."
@@ -36,7 +34,6 @@ const Projects = () => {
                 projectLink="https://www.logmein.com/products/resolve/trial"
             />
               <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={VIBLogo}
                 title="VIB Home - VIB, Belgium"
                 description="Vib-Home: A configurable micro-frontend showcasing research work, featuring advanced filters for displaying posts and news. Enhanced content discoverability, boosting user engagement by 35%. Empowered web publishers to create posts using CKEditor, where I developed custom plugins for diagrams, attachments, and a new text viewer. Accelerated content creation by 50%, enabling richer and more interactive posts."
@@ -44,7 +41,6 @@ const Projects = () => {
                 projectLink="https://home.vib.be"
             />
               <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={VIBLogo}
                 title="VIB Ticketing System - VIB, Belgium"
                 description="Built a ticketing system where users can create and assign tickets.
@@ -57,7 +53,6 @@ const Projects = () => {
                 projectLink="https://tickets.vib.be"
             />
               <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={VIBLogo}
                 title="VIB Knowledgebase - VIB, Belgium"
                 description="Built a knowledge base system with an admin panel for creating and managing articles using a custom CKEditor 5 build with tailored plugins. 
@@ -68,7 +63,6 @@ const Projects = () => {
                 projectLink="https://kb.vib.be"
             />
             <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={VIBLogo}
                 title="VIB App & Websites Catalog - VIB, Belgium"
                 description="Developed a centralized catalog platform where admins could submit software tools (with metadata like version, OS support, and descriptions) and website entries (URLs, categories, and summaries). 
@@ -78,7 +72,6 @@ const Projects = () => {
                 projectLink="https://catalog.vib.be"
             />
             <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={VIBLogo}
                 title="VIB News & Annoucements - VIB, Belgium"
                 description="Developed a versatile news and announcements portal featuring an intuitive CMS interface for admins, powered by a customizable rich-text editor (CKEditor) to effortlessly create and format articles. The pluggable frontend displayed content in a dynamic, filterable feed—organized by categories, labs, or centers—with pinned posts for priority visibility. Designed as a pluggable microfrontend, the system seamlessly integrated across platforms while maintaining user-specific filters. Additionally, integrated Google Analytics to track engagement, providing admins with actionable insights into content performance."
@@ -86,7 +79,6 @@ const Projects = () => {
                 projectLink="https://vib.be/en/news#/"
             />
             <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={VIBLogo}
                 title="VIB Email Campaigns - VIB, Belgium"
                 description="Built an email campaign management platform that enabled admins to create and send monthly digest emails highlighting news, announcements, and promotions. Integrated with Mailchimp's API for email distribution, the solution tracked user engagement through webhook callbacks that captured opens, clicks, and delivery statuses in real-time. These webhooks fed data into our PostgreSQL database, powering an analytics dashboard that displayed key metrics (delivery rates, open rates, CTR). The platform included template versioning  and subscriber list segmentation based on user preferences and engagement history."
@@ -94,7 +86,6 @@ const Projects = () => {
             />
 
             <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={AternosAutoConfirmIcon}
                 title="Aternos Auto Confirm Chrome Extension"
                 description="built a lightweight Chrome extension using vanilla JavaScript that automatically confirms Aternos Minecraft server queues the moment slots become available. The extension monitors the queue status in real-time and programmatically clicks the confirmation button, eliminating manual refreshes. With no dependencies and minimal resource usage, it runs seamlessly in the background while users play. Within just 20 days of launch, it organically gained 154 active users, proving its utility for the Aternos community."
@@ -103,16 +94,14 @@ const Projects = () => {
                 githubLink="https://github.com/PrinoyDcosta/aternos-auto-confirm-extension"
             />
             <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={PersonalProjectIcon}
                 title="React Resume Builder Component"
                 description="Developed a modular React Resume Component using ShadCN UI and Tailwind CSS, enabling dynamic customization. Users can easily add, remove, and reorder sections through intuitive drag-and-drop functionality. Built as a lightweight, reusable npm package for seamless integration. Utilizes React DnD for smooth, interactive resume editing. Maintains a clean, modern UI with a focus on accessibility. Designed for flexibility, allowing personalized resume layouts. Perfect for developers seeking a customizable, easy-to-implement solution."
                 tags={["Personal Project", "Open Source Code"]}
-                //projectLink="https://chromewebstore.google.com/detail/aternos-server-auto-confi/mfbcjmjdjfhdhkameeoljajobdgccdgf"
+                projectLink="https://www.npmjs.com/package/react-resume-viewer"
                 githubLink="https://github.com/PrinoyDcosta/react-resume-viewer"
             />
             <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={PersonalProjectIcon}
                 title="Linux-based Auto Port toggler on DDoS"
                 description="Built a Linux-based DDoS protection system for HLDS game servers that automatically toggles server ports during attacks. Using AWS Lightsail's APIs, it shuts down ports when detecting DDoS traffic through metrics analysis, then restores access when the attack subsides. The system integrates with an AMXX plugin to whitelist active players' IPs before lockdown, maintaining legitimate access while blocking malicious traffic. This automated solution combines cloud infrastructure control with game server protection, eliminating manual intervention during attacks while preserving real player connections through intelligent IP filtering."
@@ -120,7 +109,6 @@ const Projects = () => {
                 githubLink="https://github.com/PrinoyDcosta/auto-port-toggler-with-whitelister"
             />
             <CardItem
-                area="md:[grid-area:1/1/3/3] xl:[grid-area:1/1/2/6]"
                 icon={HmaiIcon}
                 title="Goa Homeopathic website"
                 description="Developed a comprehensive website for the Goa Homeopathic Association of India, featuring a public-facing landing page alongside a secure admin panel. The admin interface enabled authorized users to dynamically manage content—including uploading photos to organized albums and maintaining an updated directory of doctors with their contact details for the 'fIND Your Doctor' feature. The website also incorporated visitor analytics to track and display user traffic, providing valuable insights into engagement. Built with a responsive design, the platform effectively served both the association's promotional needs and the public's access to homeopathic practitioners in Goa."
